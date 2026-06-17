ac556f22a6f3c0dce016a931314b06376c30466d
samzmn <sam.zmn99@gmail.com>
2026-06-17 17:45:49 +0330

    register.py implemented

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Brain-Abscesses
+
+How to create brain mask using hd-bet
+
+hd-bet -i ./dataset/registered/240/FLAIR.nii.gz -o ./dataset/masked/240/FLAIR_brain.nii.gz   
---
+++ src/register.py
@@ -0,0 +1,81 @@
+import time
+import datetime
+import ants
+
+patient_id = "240"
+in_path = "./dataset/nifti"
+out_path = "./dataset/registered"
+
+start_time = time.time()
+
+# Register T2 to FLAIR
+t2_time = time.time()
+print("Registering T2 to FLAIR...")
+
+fixed = ants.image_read(f"{in_path}/{patient_id}/FLAIR.nii.gz")
+moving = ants.image_read(f"{in_path}/{patient_id}/T2.nii.gz")
+
+reg = ants.registration(
+    fixed=fixed,
+    moving=moving,
+    type_of_transform="Affine"
+)
+
+registered = reg["warpedmovout"]
+
+ants.image_write(
+    registered,
+    f"{out_path}/{patient_id}/T2_registered_to_FLAIR.nii.gz"
+)
+
+t2_time = time.time() - t2_time
+t2_time = str(datetime.timedelta(seconds=int(t2_time)))
+print(f"T2 to FLAIR registration completed in {t2_time}.")
+
+# Register T1 to FLAIR
+t1_time = time.time()
+print("Registering T1 to FLAIR...")
+
+fixed = ants.image_read(f"{in_path}/{patient_id}/FLAIR.nii.gz")
+moving = ants.image_read(f"{in_path}/{patient_id}/T1.nii.gz")
+
+reg = ants.registration(
+    fixed=fixed,
+    moving=moving,
+    type_of_transform="Affine"
+)
+
+ants.image_write(
+    reg["warpedmovout"],
+    f"{out_path}/{patient_id}/T1_registered_to_FLAIR.nii.gz"
+)
+
+t1_time = time.time() - t1_time
+t1_time = str(datetime.timedelta(seconds=int(t1_time)))
+print(f"T1 to FLAIR registration completed in {t1_time}.")
+
+# Register ADC to FLAIR
+adc_time = time.time()
+print("Registering ADC to FLAIR...")
+
+fixed = ants.image_read(f"{in_path}/{patient_id}/FLAIR.nii.gz")
+moving = ants.image_read(f"{in_path}/{patient_id}/dADC.nii.gz")
+
+reg = ants.registration(
+    fixed=fixed,
+    moving=moving,
+    type_of_transform="Affine"
+)
+
+ants.image_write(
+    reg["warpedmovout"],
+    f"{out_path}/{patient_id}/ADC_registered_to_FLAIR.nii.gz"
+)
+
+adc_time = time.time() - adc_time
+adc_time = str(datetime.timedelta(seconds=int(adc_time)))
+print(f"ADC to FLAIR registration completed in {adc_time}.")
+
+end_time = time.time() - start_time
+end_time = str(datetime.timedelta(seconds=int(end_time)))
+print(f"Total registration time: {end_time}.")
